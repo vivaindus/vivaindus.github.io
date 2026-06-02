e0aafa141e6ea646899992d64e2f8041d2981835
inventory-choco <inventory.choco@gmail.com>
2026-06-02 12:06:29 +0000

    Use direct AdSense script tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <script 
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function() {
-                var script = document.createElement('script');
-                script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8152928186282906";
-                script.async = true;
-                script.crossOrigin = "anonymous";
-                script.setAttribute('data-ad-client', 'ca-pub-8152928186282906');
-                document.head.appendChild(script);
-              })();
-            `
-          }}
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8152928186282906"
+          crossOrigin="anonymous"
         />
       </Head>
       <body>
@@ -24,5 +15,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
